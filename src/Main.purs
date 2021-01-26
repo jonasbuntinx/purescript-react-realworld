5da6b2517cb300282c43c1e0f9f432c4fe081d0b
Jonas Buntinx <buntinxjonas@gmail.com>
2021-01-27 00:44:58 +0900

    Filled out appImpl

--- src/Main.purs
+++ src/Main.purs
@@ -4,7 +4,7 @@ import Prelude
 import Apiary as Apiary
 import Conduit.Api.Endpoints as Endpoints
 import Conduit.Api.Utils (makeRequest, makeSecureRequest)
-import Conduit.AppM (AppImpl, AppM, UserApiImpl, modifyAuth, runAppM)
+import Conduit.AppM (AppImpl, AppM, modifyAuth, runAppM)
 import Conduit.Component.Auth as Auth
 import Conduit.Data.Auth (toAuth)
 import Conduit.Data.Error (Error(..))
@@ -13,7 +13,7 @@ import Conduit.Root as Root
 import Data.Either (Either(..), either)
 import Data.Maybe (Maybe(..))
 import Data.Symbol (SProxy(..))
-import Data.Variant (match)
+import Data.Variant (expand, match)
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Effect.Class (liftEffect)
@@ -50,86 +50,132 @@ main = do
                 _ -> pure unit
           }
       launchAff_ do
-        root <- runAppM appImpl Root.makeRoot
+        root <- runAppM (appImpl { navigate: router.navigate, redirect: router.redirect }) Root.makeRoot
         liftEffect $ render (React.fragment [ router.component, auth.component, root unit ]) c
 
-appImpl :: AppImpl AppM
-appImpl =
+appImpl :: { navigate :: Route -> Effect Unit, redirect :: Route -> Effect Unit } -> AppImpl AppM
+appImpl { navigate, redirect } =
   { auth:
       { readAuth: liftEffect $ Exception.throw "readAuth not implemented"
       , readAuthEvent: liftEffect $ Exception.throw "readAuthEvent not implemented"
       , modifyAuth: \_ -> liftEffect $ Exception.throw "modifyAuth not implemented"
       }
   , routing:
-      { navigate: \_ -> liftEffect $ Exception.throw "navigate not implemented"
-      , redirect: \_ -> liftEffect $ Exception.throw "redirect not implemented"
-      , logout: liftEffect $ Exception.throw " logout not implemented"
+      { navigate: liftEffect <<< navigate
+      , redirect: liftEffect <<< redirect
+      , logout:
+          do
+            void $ modifyAuth $ const Nothing
+            liftEffect $ redirect Home
       }
-  , userApi: userApiImpl
+  , userApi:
+      { loginUser:
+          \credentials -> do
+            res <- makeRequest (Apiary.Route :: Endpoints.LoginUser) Apiary.none Apiary.none { user: credentials }
+            res
+              # either
+                  (pure <<< Left)
+                  ( match
+                      { ok:
+                          \{ user: currentUser } -> do
+                            void $ modifyAuth $ const $ toAuth currentUser.token (Just $ Record.delete (SProxy :: _ "token") currentUser)
+                            pure $ Right currentUser
+                      , unprocessableEntity: pure <<< Left <<< UnprocessableEntity <<< _.errors
+                      }
+                  )
+      , registerUser:
+          \user -> do
+            res <- makeRequest (Apiary.Route :: Endpoints.RegisterUser) Apiary.none Apiary.none { user }
+            res
+              # either
+                  (pure <<< Left)
+                  ( match
+                      { ok:
+                          \{ user: currentUser } -> do
+                            void $ modifyAuth $ const $ toAuth currentUser.token (Just $ Record.delete (SProxy :: _ "token") currentUser)
+                            pure $ Right currentUser
+                      , unprocessableEntity: pure <<< Left <<< UnprocessableEntity <<< _.errors
+                      }
+                  )
+      , updateUser:
+          \user -> do
+            res <- makeSecureRequest (Apiary.Route :: Endpoints.UpdateUser) Apiary.none Apiary.none { user }
+            res
+              # either
+                  (pure <<< Left)
+                  ( match
+                      { ok:
+                          \{ user: currentUser } -> do
+                            void $ modifyAuth $ map $ _ { user = Just $ Record.delete (SProxy :: _ "token") currentUser }
+                            pure $ Right currentUser
+                      , unprocessableEntity: pure <<< Left <<< UnprocessableEntity <<< _.errors
+                      }
+                  )
+      }
   , articleApi:
-      { listArticles: \_ -> liftEffect $ Exception.throw "listArticles not implemented"
-      , listFeed: \_ -> liftEffect $ Exception.throw "listFeed not implemented"
-      , getArticle: \_ -> liftEffect $ Exception.throw "getArticle not implemented"
-      , submitArticle: \_ _ -> liftEffect $ Exception.throw "submitArticle not implemented"
-      , deleteArticle: \_ -> liftEffect $ Exception.throw "deleteArticle not implemented"
-      , toggleFavorite: \_ -> liftEffect $ Exception.throw "toggleFavorite not implemented"
+      { listArticles:
+          \query -> do
+            res <- makeRequest (Apiary.Route :: Endpoints.ListArticles) Apiary.none query Apiary.none
+            pure $ res >>= match { ok: Right }
+      , listFeed:
+          \query -> do
+            res <- makeSecureRequest (Apiary.Route :: Endpoints.ListFeed) Apiary.none query Apiary.none
+            pure $ res >>= match { ok: Right }
+      , getArticle:
+          \slug -> do
+            res <- makeRequest (Apiary.Route :: Endpoints.GetArticle) { slug } Apiary.none Apiary.none
+            pure $ res >>= (match { ok: Right <<< _.article, notFound: Left <<< NotFound })
+      , submitArticle:
+          \slug article -> do
+            res <- case slug of
+              Nothing -> map expand <$> makeSecureRequest (Apiary.Route :: Endpoints.CreateArticle) Apiary.none Apiary.none { article }
+              Just slug' -> map expand <$> makeSecureRequest (Apiary.Route :: Endpoints.UpdateArticle) { slug: slug' } Apiary.none { article }
+            pure $ res >>= (match { ok: Right <<< _.article, unprocessableEntity: Left <<< UnprocessableEntity <<< _.errors })
+      , deleteArticle:
+          \slug -> do
+            res <- makeSecureRequest (Apiary.Route :: Endpoints.DeleteArticle) { slug } Apiary.none Apiary.none
+            pure $ res >>= (match { ok: const $ Right unit })
+      , toggleFavorite:
+          \{ slug, favorited } -> do
+            res <-
+              if favorited then
+                makeSecureRequest (Apiary.Route :: Endpoints.UnfavoriteArticle) { slug } Apiary.none Apiary.none
+              else
+                makeSecureRequest (Apiary.Route :: Endpoints.FavoriteArticle) { slug } Apiary.none Apiary.none
+            pure $ res >>= match { ok: Right <<< _.article }
       }
   , commentApi:
-      { listComments: \_ -> liftEffect $ Exception.throw "listComments not implemented"
-      , createComment: \_ _ -> liftEffect $ Exception.throw "createComment not implemented"
-      , deleteComment: \_ _ -> liftEffect $ Exception.throw "deleteComment not implemented"
+      { listComments:
+          \slug -> do
+            res <- makeRequest (Apiary.Route :: Endpoints.ListComments) { slug } Apiary.none Apiary.none
+            pure $ res >>= match { ok: Right <<< _.comments }
+      , createComment:
+          \slug comment -> do
+            res <- makeSecureRequest (Apiary.Route :: Endpoints.CreateComment) { slug } Apiary.none { comment }
+            pure $ res >>= (match { ok: Right <<< _.comment })
+      , deleteComment:
+          \slug id -> do
+            res <- makeSecureRequest (Apiary.Route :: Endpoints.DeleteComment) { slug, id } Apiary.none Apiary.none
+            pure $ res >>= (match { ok: const $ Right unit })
       }
   , profileApi:
-      { getProfile: \_ -> liftEffect $ Exception.throw "getProfile not implemented"
-      , toggleFollow: \_ -> liftEffect $ Exception.throw "toggleFollow not implemented"
+      { getProfile:
+          \username -> do
+            res <- makeRequest (Apiary.Route :: Endpoints.GetProfile) { username } Apiary.none Apiary.none
+            pure $ res >>= (match { ok: Right <<< _.profile, notFound: Left <<< NotFound })
+      , toggleFollow:
+          \{ username, following } -> do
+            res <-
+              if following then
+                makeSecureRequest (Apiary.Route :: Endpoints.UnfollowProfile) { username } Apiary.none Apiary.none
+              else
+                makeSecureRequest (Apiary.Route :: Endpoints.FollowProfile) { username } Apiary.none Apiary.none
+            pure $ res >>= match { ok: Right <<< _.profile }
       }
   , tagApi:
-      { listTags: liftEffect $ Exception.throw "listTags not implemented"
+      { listTags:
+          do
+            res <- makeRequest (Apiary.Route :: Endpoints.ListTags) Apiary.none Apiary.none Apiary.none
+            pure $ res >>= match { ok: Right <<< _.tags }
       }
-  }
-
-userApiImpl :: UserApiImpl AppM
-userApiImpl =
-  { loginUser:
-      \credentials -> do
-        res <- makeRequest (Apiary.Route :: Endpoints.LoginUser) Apiary.none Apiary.none { user: credentials }
-        res
-          # either
-              (pure <<< Left)
-              ( match
-                  { ok:
-                      \{ user: currentUser } -> do
-                        void $ modifyAuth $ const $ toAuth currentUser.token (Just $ Record.delete (SProxy :: _ "token") currentUser)
-                        pure $ Right currentUser
-                  , unprocessableEntity: pure <<< Left <<< UnprocessableEntity <<< _.errors
-                  }
-              )
-  , registerUser:
-      \user -> do
-        res <- makeRequest (Apiary.Route :: Endpoints.RegisterUser) Apiary.none Apiary.none { user }
-        res
-          # either
-              (pure <<< Left)
-              ( match
-                  { ok:
-                      \{ user: currentUser } -> do
-                        void $ modifyAuth $ const $ toAuth currentUser.token (Just $ Record.delete (SProxy :: _ "token") currentUser)
-                        pure $ Right currentUser
-                  , unprocessableEntity: pure <<< Left <<< UnprocessableEntity <<< _.errors
-                  }
-              )
-  , updateUser:
-      \user -> do
-        res <- makeSecureRequest (Apiary.Route :: Endpoints.UpdateUser) Apiary.none Apiary.none { user }
-        res
-          # either
-              (pure <<< Left)
-              ( match
-                  { ok:
-                      \{ user: currentUser } -> do
-                        void $ modifyAuth $ map $ _ { user = Just $ Record.delete (SProxy :: _ "token") currentUser }
-                        pure $ Right currentUser
-                  , unprocessableEntity: pure <<< Left <<< UnprocessableEntity <<< _.errors
-                  }
-              )
   }
