53f9aa895573c016dfe50d75945f7d4c3d5a3959
Jonas Buntinx <buntinxjonas@gmail.com>
2022-10-09 23:45:51 +0900

    Use capture_ in Link

--- src/Conduit/Component/Link.purs
+++ src/Conduit/Component/Link.purs
@@ -1,12 +1,13 @@
 module Conduit.Component.Link where
 
 import Prelude
+
 import Conduit.Data.Route (Route, routeCodec)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Uncurried (runEffectFn1)
 import React.Basic.DOM as R
-import React.Basic.DOM.Events (altKey, button, ctrlKey, metaKey, preventDefault, shiftKey, stopPropagation)
+import React.Basic.DOM.Events (altKey, button, capture_, ctrlKey, metaKey, shiftKey, stopPropagation)
 import React.Basic.Events (handler, merge, syntheticEvent)
 import React.Basic.Hooks as React
 import Routing.Duplex (print)
@@ -28,7 +29,7 @@ link props =
           case button, metaKey, altKey, ctrlKey, shiftKey of
             Just 0, Just false, Just false, Just false, Just false ->
               runEffectFn1
-                (handler (stopPropagation <<< preventDefault) $ \_ -> props.onClick props.route)
+                (capture_ $ props.onClick props.route)
                 syntheticEvent
             _, _, _, _, _ ->
               runEffectFn1
